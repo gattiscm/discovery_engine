746d8e6e16974ca88a0d6e0585dbbe0d4cc89393
gattiscm <137558421+gattiscm@users.noreply.github.com>
2026-06-05 15:42:17 -0500

    Update base_extractor.py
    Cleaned for readability and added comments.

--- features/base_extractor.py
+++ features/base_extractor.py
@@ -1,7 +1,23 @@
-class BaseExtractor:
+# ######################################################################
+# Author: Cameron M. Gattis
+# Created: 2026-06-05
+# Updated: New.
+# Purpose: Default use class for initialization.
+# ######################################################################
 
-    def extract(
-            self,
-            x
-    ):
+class BaseExtractor:
+    # ######################################################################
+    # Author: Cameron M. Gattis
+    # Created: 2026-06-05
+    # Updated: New.
+    # Purpose: Placeholder class that is over written by individual
+    #          extractors as needed.
+    # ######################################################################
+    def extract(self,x):
+        '''
+            Base extraction feature, not used for production.
+
+            @exception NotImplementedError : none
+        '''
+
         raise NotImplementedError
