a09cbd08f4e0120959974abac542114325a487f7
gattiscm <137558421+gattiscm@users.noreply.github.com>
2026-06-07 17:54:49 -0500

    updated exports
    cleaned and commented export scripts

--- engine/results/exporter.py
+++ engine/results/exporter.py
@@ -1,159 +1,75 @@
+# ######################################################################
+# Author: Cameron M. Gattis
+# Created: 2026-06-07
+# Updated: New.
+# Purpose: Provides a csv and JSON export capability for data.
+# ######################################################################
+
 import json
 from pathlib import Path
 import numpy as np
 import pandas as pd
 
 
-RESULTS_DIR = Path(
-    "results"
-)
+RESULTS_DIR = Path("results")
+RESULTS_DIR.mkdir(exist_ok=True)
 
-RESULTS_DIR.mkdir(
-    exist_ok=True
-)
+def sanitize_json(obj):
+    '''
+        Provides JSON-safe conversion.
 
-def json_converter(
-        o
-):
+        @params obj : multitype
 
-    if isinstance(
-            o,
-            np.integer
-    ):
-
-        return int(o)
-
-    if isinstance(
-            o,
-            np.floating
-    ):
-
-        return float(o)
-
-    if isinstance(
-            o,
-            np.ndarray
-    ):
-
-        return o.tolist()
-
-    return str(o)
-
-def sanitize_json(
-        obj
-):
-
-    if isinstance(
-            obj,
-            dict
-    ):
-
+        @return obj : multitype
+    '''
+    if isinstance(obj,dict):
         return {
-
-            str(
-                sanitize_json(k)
-            ):
-
+            str(sanitize_json(k)):
             sanitize_json(v)
-
             for k,v in obj.items()
         }
-
-    elif isinstance(
-            obj,
-            list
-    ):
-
+    elif isinstance(obj,list):
         return [
-
             sanitize_json(x)
-
             for x in obj
         ]
-
-    elif isinstance(
-            obj,
-            tuple
-    ):
-
+    elif isinstance(obj,tuple):
         return tuple(
-
             sanitize_json(x)
-
             for x in obj
         )
-
-    elif isinstance(
-            obj,
-            np.integer
-    ):
+    elif isinstance(obj,np.integer):
 
         return int(obj)
-
-    elif isinstance(
-            obj,
-            np.floating
-    ):
-
+    elif isinstance(obj,np.floating):
         return float(obj)
 
-    elif isinstance(
-            obj,
-            np.ndarray
-    ):
-
+    elif isinstance(obj,np.ndarray):
         return obj.tolist()
 
     return obj
 
-def save_csv(
-        df,
-        filename
-):
+def save_csv(df,filename):
+    '''
+        Exports data to csv.
 
-    path = (
-        RESULTS_DIR /
-        filename
-    )
-
-    df.to_csv(
-        path,
-        index=False
-    )
-
-    print(
-        f"Saved: {path}"
-    )
+        @params df : dataframe
+                filename : string
+    '''
+    path = (RESULTS_DIR / filename)
+    df.to_csv(path,index=False)
+    print(f"Saved: {path}")
 
 
-def save_json(
-        obj,
-        filename
-):
+def save_json(obj,filename):
+    '''
+        Exports data and cleans the data to JSON.
 
-    path = (
-        RESULTS_DIR /
-        filename
-    )
-
-    with open(
-            path,
-            "w"
-    ) as f:
-
-        clean_obj = sanitize_json(
-            obj
-        )
-
-        json.dump(
-
-            clean_obj,
-            f,
-
-            indent=4
-
-        )
-
-    print(
-        f"Saved: {path}"
-    )
+        @params df : object
+                filename : string
+    '''
+    path = (RESULTS_DIR /filename)
+    with open(path,"w") as f:
+        clean_obj = sanitize_json(obj)
+        json.dump(clean_obj,f,indent=4)
+    print(f"Saved: {path}")
--- engine/results/metadata.py
+++ engine/results/metadata.py
@@ -1,23 +1,24 @@
-from datetime import datetime
+# ######################################################################
+# Author: Cameron M. Gattis
+# Created: 2026-06-07
+# Updated: New.
+# Purpose: Provides a JSON export capability for metadata.
+# ######################################################################
 
+from datetime import datetime
 from engine.results.exporter import save_json
 
 
-def save_run_metadata(
-        filename="run_metadata.json",
-        **kwargs
-):
+def save_run_metadata(filename="run_metadata.json",**kwargs):
+    '''
+        Purpose of function/method.
 
+        @params timestamp : datetime object
+                kwargs    : misc data
+    '''
     metadata = {
-
-        "timestamp":
-            datetime.now().isoformat(),
-
+        "timestamp": datetime.now().isoformat(),
         **kwargs
-
     }
 
-    save_json(
-        metadata,
-        filename
-    )
+    save_json(metadata,filename)
