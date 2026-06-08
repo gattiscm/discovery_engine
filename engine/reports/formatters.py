3ae60a03dd434c1034a3eeffdbfaf99e814c997c
gattiscm <137558421+gattiscm@users.noreply.github.com>
2026-06-08 09:30:21 -0500

    Update formatters.py
    Added comment header to empty file explaining future expansion.

---
+++ engine/reports/formatters.py
@@ -0,0 +1,8 @@
+# ######################################################################
+# Author: Cameron M. Gattis
+# Created: 2026-06-08
+# Updated: Replaced.
+# Purpose: Placeholder script for architectural purposes that will be
+#          used to format various report outputs into text, csv, JSON,
+#          and LaTex
+# ######################################################################
