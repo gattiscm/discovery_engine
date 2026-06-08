d23e5f9a468fe1e39310271e1a46516f9a613ff9
gattiscm <137558421+gattiscm@users.noreply.github.com>
2026-06-08 09:30:16 -0500

    Update comparison_report.py
    Added comment header to empty file explaining future expansion.

---
+++ engine/reports/comparison_report.py
@@ -0,0 +1,7 @@
+# ######################################################################
+# Author: Cameron M. Gattis
+# Created: 2026-06-08
+# Updated: Replaced.
+# Purpose: Placeholder script for architectural purposes that will be
+#          used to compare outputs and runs.
+# ######################################################################
