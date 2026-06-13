c1aa0f2a7295c362293014e9b09b501776706f9d
gattiscm <137558421+gattiscm@users.noreply.github.com>
2026-06-13 17:10:06 -0500

    Update base.py
    Updated for readability and comments.

--- engine/novelty_methods/base.py
+++ engine/novelty_methods/base.py
@@ -1,21 +1,50 @@
+# ######################################################################
+# Author: Cameron M. Gattis
+# Created: 2026-06-13
+# Updated: New.
+# Purpose: Base interface for novelty scoring methods.
+# ######################################################################
+
+
 class BaseNovelty:
+    # ##################################################################
+    # Author: Cameron M. Gattis
+    # Created: 2026-06-13
+    # Updated: New.
+    # Purpose: Defines the standard interface used by all
+    #          novelty scoring implementations.
+    # ##################################################################
 
-    def fit(
-            self,
-            X
-    ):
+    def fit(self, X):
+        '''
+            Learns any required statistics from
+            training data.
+
+            @params X : numpy array
+        '''
         pass
 
-    def compute(
-            self,
-            x,
-            reconstruction
-    ):
+
+    def compute(self, x, reconstruction):
+        '''
+            Computes novelty score.
+
+            @params x : numpy array
+                    reconstruction : numpy array
+
+            @return novelty : float
+        '''
         pass
 
-    def explain(
-            self,
-            x,
-            reconstruction
-    ):
+
+    def explain(self, x, reconstruction):
+        '''
+            Returns feature-level explanation of
+            novelty score.
+
+            @params x : numpy array
+                    reconstruction : numpy array
+
+            @return explanation : object
+        '''
         pass
