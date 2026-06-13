c4caa9730f5273145ede2f6f205ba898f45c9012
gattiscm <137558421+gattiscm@users.noreply.github.com>
2026-06-13 17:10:10 -0500

    Update ensemble.py
    Updated for readability and comments.

--- engine/novelty_methods/ensemble.py
+++ engine/novelty_methods/ensemble.py
@@ -1,79 +1,84 @@
+# ######################################################################
+# Author: Cameron M. Gattis
+# Created: 2026-06-13
+# Updated: New.
+# Purpose: Combines multiple novelty scoring methods into a
+#          weighted ensemble score.
+# ######################################################################
+
 import numpy as np
 
 from .base import BaseNovelty
 
 
-class EnsembleNovelty(
-        BaseNovelty
-):
+class EnsembleNovelty(BaseNovelty):
+    # ##################################################################
+    # Author: Cameron M. Gattis
+    # Created: 2026-06-13
+    # Updated: New.
+    # Purpose: Aggregates novelty scores from multiple methods
+    #          using weighted summation.
+    # ##################################################################
 
-    def __init__(
-            self,
-            methods
-    ):
+    def __init__(self, methods):
+        '''
+            Initializes ensemble novelty scorer.
 
+            @params methods : list
+                List of tuples in the form:
+
+                (novelty_method, weight)
+        '''
         self.methods = methods
 
 
-    def fit(
-            self,
-            X
-    ):
+    def fit(self, X):
+        '''
+            Fits all novelty methods in ensemble.
 
+            @params X : numpy array
+        '''
         for method, weight in self.methods:
-
-            method.fit(
-                X
-            )
+            method.fit(X)
 
 
-    def compute(
-            self,
-            x,
-            reconstruction
-    ):
+    def compute(self, x, reconstruction):
+        '''
+            Computes weighted ensemble novelty score.
 
+            @params x : numpy array
+                    reconstruction : numpy array
+
+            @return novelty : float
+        '''
         total = 0.0
 
         for method, weight in self.methods:
-
-            score = method.compute(
-                x,
-                reconstruction
-            )
-
-            total += (
-                weight *
-                score
-            )
+            score = method.compute(x,reconstruction)
+            total += (weight * score)
 
         return total
 
 
-    def explain(
-            self,
-            x,
-            reconstruction
-    ):
+    def explain(self, x, reconstruction):
+        '''
+            Returns contribution from each novelty
+            scoring method.
 
+            @params x : numpy array
+                    reconstruction : numpy array
+
+            @return explanation : dict
+        '''
         explanation = {}
 
         for method, weight in self.methods:
 
-            name = (
-                method.__class__.__name__
-            )
+            name = method.__class__.__name__
 
             explanation[name] = {
-
-                "weight":
-                    weight,
-
-                "score":
-                    method.compute(
-                        x,
-                        reconstruction
-                    )
+                "weight": weight,
+                "score": method.compute(x,reconstruction)
             }
 
         return explanation
